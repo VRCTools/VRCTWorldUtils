4339f45e3c646769f3ab5ea9956048c7045b0c0a
Yuki Donath <dotstart@pm.me>
2025-01-25 19:27:45 +0100

    Added a base editor implementation for use with Udon scripts.

---
+++ Packages/io.vrctools.world/Editor/Scripts/Abstractions/AbstractCustomUdonEditor.cs
@@ -0,0 +1,15 @@
+﻿using UdonSharpEditor;
+
+namespace VRCTools.World.Editor.Abstractions {
+  /// <summary>
+  /// Provides a specialized variation of the abstract custom editor implementation which includes Udon related UI
+  /// elements by default.
+  /// </summary>
+  public abstract class AbstractCustomUdonEditor : AbstractCustomEditor {
+    public override void OnInspectorGUI() {
+      if (UdonSharpGUI.DrawDefaultUdonSharpBehaviourHeader(this.target)) return;
+
+      base.OnInspectorGUI();
+    }
+  }
+}
