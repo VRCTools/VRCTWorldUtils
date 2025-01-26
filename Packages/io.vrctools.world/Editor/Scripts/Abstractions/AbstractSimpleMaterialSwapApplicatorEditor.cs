ba24140d6f554bbc095380155587d8317f54b1bc
Yuki Donath <dotstart@pm.me>
2025-01-26 16:18:18 +0100

    Added support for simple material swaps.

---
+++ Packages/io.vrctools.world/Editor/Scripts/Abstractions/AbstractSimpleMaterialSwapApplicatorEditor.cs
@@ -0,0 +1,56 @@
+﻿// Copyright 2025 .start <https://dotstart.tv>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using UnityEditor;
+using UnityEngine;
+using VRCTools.World.Abstractions;
+using VRCTools.World.Editor.Utils;
+
+namespace VRCTools.World.Editor.Abstractions {
+  using Applicator = AbstractSimpleMaterialSwapApplicator;
+
+  public abstract class AbstractSimpleMaterialSwapApplicatorEditor : AbstractCustomUdonEditor {
+    private SerializedProperty _renderers;
+    private SerializedProperty _slotIndices;
+    private SerializedProperty _replacementMaterials;
+
+    private SerializedProperty _invert;
+
+    private MultiPropertyList _rendererList;
+
+    protected virtual void OnEnable() {
+      this._renderers = this.serializedObject.FindProperty(nameof(Applicator.renderers));
+      this._slotIndices = this.serializedObject.FindProperty(nameof(Applicator.slotIndices));
+      this._replacementMaterials = this.serializedObject.FindProperty(nameof(Applicator.replacementMaterials));
+
+      this._invert = this.serializedObject.FindProperty(nameof(Applicator.invert));
+
+      this._rendererList = new MultiPropertyList(
+        new GUIContent("Swaps"),
+        this.serializedObject,
+        "Renderer",
+        this._renderers,
+        new[] { "Index", "Replacement" },
+        new[] { this._slotIndices, this._replacementMaterials });
+    }
+
+    protected override void RenderInspectorGUI() {
+      this._rendererList.DoLayout();
+      EditorGUILayout.Space(20);
+
+      EditorGUILayout.LabelField("Advanced Settings");
+      EditorGUILayout.PropertyField(this._invert);
+    }
+  }
+}
---
+++ Packages/io.vrctools.world/Runtime/UdonScripts/Abstractions/AbstractSimpleMaterialSwapApplicator.cs
@@ -0,0 +1,63 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRCTools.World.LocalValues;
+using VRCTools.World.Utils;
+
+namespace VRCTools.World.Abstractions {
+  public abstract class AbstractSimpleMaterialSwapApplicator : UdonSharpBehaviour {
+    public Renderer[] renderers;
+    public int[] slotIndices;
+    public Material[] replacementMaterials;
+
+    public bool invert;
+
+    private Material[] _originalMaterials;
+
+    protected abstract bool State { get; }
+
+    protected virtual void Start() {
+      this.renderers = MappingUtility.CheckCoherentMapping(this.renderers, this.slotIndices, this);
+      this.renderers = MappingUtility.CheckCoherentMapping(this.renderers, this.replacementMaterials, this);
+
+      this._originalMaterials = new Material[this.renderers.Length];
+      for (var i = 0; i < this.renderers.Length; i++) {
+        var r = this.renderers[i];
+        var slot = this.slotIndices[i];
+
+        if (slot >= r.sharedMaterials.Length) {
+          Debug.LogWarning(
+            $"[Simple Material Swap Applicator] Desired slot {slot} exceeds allocated material list length", r);
+          continue;
+        }
+
+        this._originalMaterials[i] = r.sharedMaterials[slot];
+      }
+    }
+
+    public void _OnStateUpdated() {
+      var value = this.State ^ this.invert;
+
+      for (var i = 0; i < this.renderers.Length; i++) {
+        var r = this.renderers[i];
+        var slot = this.slotIndices[i];
+        var replacementMaterial = this.replacementMaterials[slot];
+        var originalMaterial = this._originalMaterials[slot];
+
+        if (!Utilities.IsValid(r) || slot < 0) {
+          continue;
+        }
+
+        if (slot >= r.sharedMaterials.Length) {
+          Debug.LogWarning(
+            $"[Simple Material Swap Applicator] Desired slot {slot} exceeds allocated material list length", r);
+          continue;
+        }
+
+        var materials = r.sharedMaterials;
+        materials[i] = value ? replacementMaterial : originalMaterial;
+        r.sharedMaterials = materials;
+      }
+    }
+  }
+}
