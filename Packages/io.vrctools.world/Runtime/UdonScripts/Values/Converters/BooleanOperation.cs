5aee7362ab18a320e68baa397a4a7c37ae69f0fd
.start <dotstart@pm.me>
2025-02-15 21:19:29 +0100

    Added support for boolean operations.

---
+++ Packages/io.vrctools.world/Runtime/UdonScripts/Values/Converters/BooleanOperation.cs
@@ -0,0 +1,11 @@
+﻿namespace VRCTools.World.Values.Converters {
+  public enum BooleanOperation {
+    NOT = 0,
+    AND = 1,
+    OR = 2,
+    XOR = 3,
+    NAND = 4,
+    NOR = 5,
+    XNOR = 6,
+  }
+}
---
+++ Packages/io.vrctools.world/Runtime/UdonScripts/Values/Converters/BooleanOperationConverter.cs
@@ -0,0 +1,127 @@
+﻿// Copyright 2025 .start <https://dotstart.tv>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRCTools.World.LocalValues;
+using VRCTools.World.SynchronizedValues;
+
+namespace VRCTools.World.Values.Converters {
+  [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+  [AddComponentMenu("Values/Converters/Boolean Operation Converter")]
+  public class BooleanOperationConverter : UdonSharpBehaviour {
+    public bool useSynchronizedTarget;
+    public LocalBoolean localTarget;
+    public SynchronizedBoolean synchronizedTarget;
+
+    public BooleanOperation operation;
+
+    public ValueSource sourceAlpha;
+    public bool staticAlphaValue;
+    public LocalBoolean localAlphaValue;
+    public SynchronizedBoolean synchronizedAlphaValue;
+
+    public ValueSource sourceBeta;
+    public bool staticBetaValue;
+    public LocalBoolean localBetaValue;
+    public SynchronizedBoolean synchronizedBetaValue;
+
+    private void Start() {
+      if (this.useSynchronizedTarget) {
+        if (!Utilities.IsValid(this.synchronizedTarget)) {
+          Debug.LogError("[Boolean Operation Converter] Synchronized target is invalid - Disabled", this);
+          this.enabled = false;
+          return;
+        }
+      } else if (!Utilities.IsValid(this.localTarget)) {
+        Debug.LogError("[Boolean Operation Converter] Local target is invalid - Disabled", this);
+        this.enabled = false;
+        return;
+      }
+
+      this._RegisterHandler(this.sourceAlpha, this.localAlphaValue, this.synchronizedAlphaValue);
+      this._RegisterHandler(this.sourceBeta, this.localBetaValue, this.synchronizedBetaValue);
+
+      this._OnComponentUpdated();
+    }
+
+    private void _RegisterHandler(
+      ValueSource source,
+      LocalBoolean localValue,
+      SynchronizedBoolean synchronizedValue) {
+      switch (source) {
+        case ValueSource.STATIC: break;
+        case ValueSource.LOCAL:
+          localValue._RegisterHandler(LocalBoolean.EVENT_STATE_UPDATED, this, nameof(this._OnComponentUpdated));
+          break;
+        case ValueSource.SYNCHRONIZED:
+          synchronizedValue._RegisterHandler(SynchronizedBoolean.EVENT_STATE_UPDATED, this,
+            nameof(this._OnComponentUpdated));
+          break;
+      }
+    }
+
+    public void _OnComponentUpdated() {
+      var alpha = this._GetValue(this.sourceAlpha, this.staticAlphaValue, this.localAlphaValue,
+        this.synchronizedAlphaValue);
+      var beta = this._GetValue(this.sourceBeta, this.staticBetaValue, this.localBetaValue, this.synchronizedBetaValue);
+
+      var newValue = false;
+      switch (this.operation) {
+        case BooleanOperation.NOT:
+          newValue = !alpha;
+          break;
+        case BooleanOperation.AND:
+          newValue = alpha && beta;
+          break;
+        case BooleanOperation.OR:
+          newValue = alpha || beta;
+          break;
+        case BooleanOperation.XOR:
+          newValue = alpha ^ beta;
+          break;
+        case BooleanOperation.NAND:
+          newValue = !(alpha || beta);
+          break;
+        case BooleanOperation.NOR:
+          newValue = !(alpha || beta);
+          break;
+        case BooleanOperation.XNOR:
+          newValue = !(alpha ^ beta);
+          break;
+      }
+
+      if (this.useSynchronizedTarget) {
+        this.synchronizedTarget.State = newValue;
+      } else {
+        this.localTarget.State = newValue;
+      }
+    }
+
+    private bool _GetValue(
+      ValueSource source,
+      bool staticValue,
+      LocalBoolean localValue,
+      SynchronizedBoolean synchronizedValue) {
+      switch (source) {
+        case ValueSource.STATIC: return staticValue;
+        case ValueSource.LOCAL: return localValue.State;
+        case ValueSource.SYNCHRONIZED: return synchronizedValue.State;
+      }
+
+      return false;
+    }
+  }
+}
