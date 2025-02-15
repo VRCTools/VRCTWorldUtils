ee4bc04833d670c87d3fc67c8918e1ec613ce0b5
.start <dotstart@pm.me>
2025-02-15 21:19:19 +0100

    Implemented presets for all relevant value types.

---
+++ Packages/io.vrctools.world/Runtime/UdonScripts/Values/Presets/FloatPreset.cs
@@ -0,0 +1,66 @@
+﻿// Copyright 2025 .start <https://dotstart.tv>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRCTools.World.LocalValues;
+using VRCTools.World.SynchronizedValues;
+
+namespace VRCTools.World.Values.Presets {
+  /// <summary>
+  ///   Stores a preset which may be applied to a given float value.
+  /// </summary>
+  [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+  [AddComponentMenu("Values/Presets/Float Preset")]
+  public class FloatPreset : UdonSharpBehaviour {
+    public bool useSynchronizedTarget;
+    public LocalFloat localTarget;
+    public SynchronizedFloat synchronizedTarget;
+
+    public float preset;
+
+    private void Start() {
+      if (this.useSynchronizedTarget) {
+        if (Utilities.IsValid(this.synchronizedTarget)) return;
+
+        Debug.LogError("[Float Preset] Synchronized target is invalid - Disabled", this);
+        this.enabled = false;
+      } else if (!Utilities.IsValid(this.localTarget)) {
+        Debug.LogError("[Float Preset] Local target is invalid - Disabled", this);
+        this.enabled = false;
+      }
+    }
+
+    public void _Apply() {
+      if (this.useSynchronizedTarget) {
+        if (!Utilities.IsValid(this.synchronizedTarget)) {
+          Debug.LogError("[Float Preset] Synchronized target is invalid - Ignored", this);
+          this.enabled = false;
+          return;
+        }
+
+        this.synchronizedTarget.State = this.preset;
+      } else {
+        if (!Utilities.IsValid(this.localTarget)) {
+          Debug.LogError("[Float Preset] Local target is invalid - Ignored", this);
+          this.enabled = false;
+          return;
+        }
+
+        this.localTarget.State = this.preset;
+      }
+    }
+  }
+}
---
+++ Packages/io.vrctools.world/Runtime/UdonScripts/Values/Presets/Vector3Preset.cs
@@ -0,0 +1,66 @@
+﻿// Copyright 2025 .start <https://dotstart.tv>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRCTools.World.LocalValues;
+using VRCTools.World.SynchronizedValues;
+
+namespace VRCTools.World.Values.Presets {
+  /// <summary>
+  ///   Stores a preset which may be applied to a given Vector3 value.
+  /// </summary>
+  [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+  [AddComponentMenu("Values/Presets/Vector3 Preset")]
+  public class Vector3Preset : UdonSharpBehaviour {
+    public bool useSynchronizedTarget;
+    public LocalVector3 localTarget;
+    public SynchronizedVector3 synchronizedTarget;
+
+    public Vector3 preset;
+
+    private void Start() {
+      if (this.useSynchronizedTarget) {
+        if (Utilities.IsValid(this.synchronizedTarget)) return;
+
+        Debug.LogError("[Vector3 Preset] Synchronized target is invalid - Disabled", this);
+        this.enabled = false;
+      } else if (!Utilities.IsValid(this.localTarget)) {
+        Debug.LogError("[Vector3 Preset] Local target is invalid - Disabled", this);
+        this.enabled = false;
+      }
+    }
+
+    public void _Apply() {
+      if (this.useSynchronizedTarget) {
+        if (!Utilities.IsValid(this.synchronizedTarget)) {
+          Debug.LogError("[Vector3 Preset] Synchronized target is invalid - Ignored", this);
+          this.enabled = false;
+          return;
+        }
+
+        this.synchronizedTarget.State = this.preset;
+      } else {
+        if (!Utilities.IsValid(this.localTarget)) {
+          Debug.LogError("[Vector3 Preset] Local target is invalid - Ignored", this);
+          this.enabled = false;
+          return;
+        }
+
+        this.localTarget.State = this.preset;
+      }
+    }
+  }
+}
