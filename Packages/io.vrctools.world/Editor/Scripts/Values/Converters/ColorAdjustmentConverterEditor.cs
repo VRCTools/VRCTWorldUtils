429aa74fb7fab6038d6d762f44b6ea447111d104
.start <dotstart@pm.me>
2025-02-17 18:50:00 +0100

    Created a color adjustment converter.

---
+++ Packages/io.vrctools.world/Editor/Scripts/Values/Converters/ColorAdjustmentConverterEditor.cs
@@ -0,0 +1,69 @@
+﻿// Copyright 2025 .start <https://dotstart.tv>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using UnityEditor;
+using UnityEngine;
+using VRCTools.World.Editor.Abstractions;
+using VRCTools.World.Editor.Utils;
+using VRCTools.World.Values.Converters;
+
+namespace VRCTools.World.Editor.Values.Converters {
+  [CustomEditor(typeof(ColorAdjustmentConverter))]
+  public class ColorAdjustmentConverterEditor : AbstractCustomUdonEditor {
+    private SerializedProperty _source;
+    private SerializedProperty _localSource;
+    private SerializedProperty _synchronizedSource;
+
+    private SerializedProperty _target;
+    private SerializedProperty _localTarget;
+    private SerializedProperty _synchronizedTarget;
+
+    private SerializedProperty _hueOffset;
+    private SerializedProperty _saturationOffset;
+    private SerializedProperty _valueOffset;
+
+    private void OnEnable() {
+      this._source = this.serializedObject.FindProperty(nameof(ColorAdjustmentConverter.source));
+      this._localSource = this.serializedObject.FindProperty(nameof(ColorAdjustmentConverter.localSource));
+      this._synchronizedSource
+        = this.serializedObject.FindProperty(nameof(ColorAdjustmentConverter.synchronizedSource));
+
+      this._target = this.serializedObject.FindProperty(nameof(ColorAdjustmentConverter.target));
+      this._localTarget = this.serializedObject.FindProperty(nameof(ColorAdjustmentConverter.localTarget));
+      this._synchronizedTarget
+        = this.serializedObject.FindProperty(nameof(ColorAdjustmentConverter.synchronizedTarget));
+
+      this._hueOffset = this.serializedObject.FindProperty(nameof(ColorAdjustmentConverter.hueOffset));
+      this._saturationOffset = this.serializedObject.FindProperty(nameof(ColorAdjustmentConverter.saturationOffset));
+      this._valueOffset = this.serializedObject.FindProperty(nameof(ColorAdjustmentConverter.valueOffset));
+    }
+
+    protected override void RenderInspectorGUI() {
+      EditorGUILayout.LabelField("Source Value", EditorStyles.boldLabel);
+      ValueEditorUtility.DrawSourceProperty(new GUIContent("Source"), this._source, this._localSource,
+        this._synchronizedSource, "Source value is invalid or unset - Component will be disabled on startup.");
+      EditorGUILayout.Space(20);
+
+      EditorGUILayout.LabelField("Target Value", EditorStyles.boldLabel);
+      ValueEditorUtility.DrawSourceProperty(new GUIContent("Target"), this._target, this._localTarget,
+        this._synchronizedTarget, "Target value is invalid or unset - Component will be disabled on startup.");
+      EditorGUILayout.Space(20);
+
+      EditorGUILayout.LabelField("Adjustment", EditorStyles.boldLabel);
+      EditorGUILayout.PropertyField(this._hueOffset);
+      EditorGUILayout.PropertyField(this._saturationOffset);
+      EditorGUILayout.PropertyField(this._valueOffset);
+    }
+  }
+}
---
+++ Packages/io.vrctools.world/Runtime/UdonScripts/Values/Converters/ColorAdjustmentConverter.cs
@@ -0,0 +1,72 @@
+﻿// Copyright 2025 .start <https://dotstart.tv>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using UdonSharp;
+using UnityEngine;
+using VRCTools.World.LocalValues;
+using VRCTools.World.SynchronizedValues;
+using VRCTools.World.Utils;
+
+namespace VRCTools.World.Values.Converters {
+  [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+  [AddComponentMenu("Values/Converters/Color Adjustment Converter")]
+  public class ColorAdjustmentConverter : UdonSharpBehaviour {
+    public ValueType source;
+    public LocalColor localSource;
+    public SynchronizedColor synchronizedSource;
+
+    public ValueType target;
+    public LocalColor localTarget;
+    public SynchronizedColor synchronizedTarget;
+
+    [Range(-1, 1)]
+    public float hueOffset;
+    [Range(-1, 1)]
+    public float saturationOffset;
+    [Range(-1, 1)]
+    public float valueOffset;
+
+    private void Start() {
+      if (!ValueUtility.IsValid(this.source, this.localSource, this.synchronizedSource)) {
+        Debug.LogError("[Color Adjustment Converter] Source is not valid - Disabled");
+        this.enabled = false;
+        return;
+      }
+
+      if (!ValueUtility.IsValid(this.target, this.localTarget, this.synchronizedTarget)) {
+        Debug.LogError("[Color Adjustment Converter] Target is not valid - Disabled");
+        this.enabled = false;
+        return;
+      }
+
+      ValueUtility.RegisterUpdateHandler(this.source, this.localSource, this.synchronizedSource, this,
+        nameof(this._OnSourceUpdated));
+
+      this.SendCustomEventDelayedFrames(nameof(this._OnSourceUpdated), 1);
+    }
+
+    public void _OnSourceUpdated() {
+      var color = ValueUtility.GetValue(this.source, this.localSource, this.synchronizedSource);
+
+      Color.RGBToHSV(color, out var h, out var s, out var v);
+
+      h += this.hueOffset;
+      s = Mathf.Clamp01(s + this.saturationOffset);
+      v = Mathf.Clamp01(v + this.valueOffset);
+
+      color = Color.HSVToRGB(h, s, v);
+      ValueUtility.SetValue(this.target, this.localTarget, this.synchronizedTarget, color);
+    }
+  }
+}
